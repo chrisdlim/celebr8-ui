3dcdf04bee51105822cbcc3a0241fb286af9d89e
christophlim <christopherdouglaslim@gmail.com>
2021-02-06 18:19:18 -0500

    initial set up with a service

---
+++ src/components/Celebrations.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <h3>Celebrations Component</h3>
+    {{ celebrations }}
+  </div>
+</template>
+
+<script>
+
+import CelebrationsService from '../services/celebrations.service';
+const celebrations = null;
+
+export default {
+  name: 'Celebrations',
+  props: {
+    msg: String
+  },
+  data () {
+    return {
+      celebrations,
+    }
+  },
+  async mounted () {
+    this.getCelebrations();
+  },
+  methods: {
+    async getCelebrations() {
+      this.celebrations = await CelebrationsService.getCelebrations().then(res => res.data)
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
